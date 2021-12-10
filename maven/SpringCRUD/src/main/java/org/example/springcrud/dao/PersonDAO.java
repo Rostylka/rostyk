71f302424d5674543f150b5c26e6f3459735c787
Rostyslav <rostylka96@gmail.com>
2021-12-10 17:39:27 +0200

    SpringCRUD

---
+++ maven/SpringCRUD/src/main/java/org/example/springcrud/dao/PersonDAO.java
@@ -0,0 +1,31 @@
+package org.example.springcrud.dao;
+
+import org.example.springcrud.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+
+    List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Nick"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Jack"));
+
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
