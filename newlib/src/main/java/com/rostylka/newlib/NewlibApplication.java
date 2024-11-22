118a94f50e4302672c2d39b13ed14f058140a6e9
Rostylka <rostylka96@gmail.com>
2024-11-22 15:40:41 +0200

    refresh

---
+++ newlib/src/main/java/com/rostylka/newlib/NewlibApplication.java
@@ -0,0 +1,13 @@
+package com.rostylka.newlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewlibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewlibApplication.class, args);
+    }
+
+}
--- src/main/java/com/rostylka/newlib/models/LogBook.java
+++
@@ -1,4 +0,0 @@
-package com.rostylka.newlib.models;
-
-public class LogBook {
-}
