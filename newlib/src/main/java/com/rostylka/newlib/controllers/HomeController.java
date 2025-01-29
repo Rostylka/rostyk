634093bf96057218da507655123350af258fc999
Rostylka <rostylka96@gmail.com>
2025-01-29 17:33:06 +0200

    Added Registration

--- newlib/src/main/java/com/rostylka/newlib/controllers/HomeController.java
+++ newlib/src/main/java/com/rostylka/newlib/controllers/HomeController.java
@@ -23,6 +23,7 @@ public class HomeController {
     public String home(Model model){
         model.addAttribute("books", bookServiceImplementation.readAllBooks());
         model.addAttribute("link", userServiceImplementation.createLink());
+        model.addAttribute("isAuthenticated", userServiceImplementation.getUserDetail()!=null);
         return "index";
     }
 
